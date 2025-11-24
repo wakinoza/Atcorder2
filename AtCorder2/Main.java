604654d1064c9007f901c781f6927d7ebd6a4eba
wakinoza <wakinoza@gmail.com>
2025-11-24 15:21:04 +0900

    Main.java

---
+++ AtCorder2/Main.java
@@ -0,0 +1,28 @@
+package AtCorder2;
+
+import java.util.*;
+import java.util.stream.*;
+
+//ABC
+//public class ABC   {
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        scanner.close();
+    }
+}
+
+        int n = scanner.nextInt();
+        String s = scanner.next();
+        char[] sArray = scanner.next().toCharArray();
+
+        System.out.println();
+        if () {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+
