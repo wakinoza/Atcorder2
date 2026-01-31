db8f0c4bc769e2eb43419ed7072777d90276749e
wakinoza <wakinoza@gmail.com>
2026-01-31 21:03:12 +0900

    ABC443A

---
+++ AtCorder2/ABC443A.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.stream.*;
+
+//ABC443A
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String s = scanner.next();
+    scanner.close();
+    System.out.println(s + "s");
+  }
+}
