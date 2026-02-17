d2ab6deaa8700b534b8d06698b11471fd65a7014
wakinoza <wakinoza@gmail.com>
2026-02-17 10:35:35 +0900

    ABC444A.py

---
+++ AtCorder2/ABC444A.py
@@ -0,0 +1,11 @@
+# ABC444A
+
+# import sys
+# sys.setrecursionlimit(10**7)
+# input = sys.stdin.readline
+
+s = input()
+if s[0] == s[1] == s[2]:
+  print("Yes")
+else :
+  print("No")
---
+++ AtCorder2/main.py
@@ -0,0 +1,13 @@
+# ABC
+# import sys
+# sys.setrecursionlimit(10**7)
+# input = sys.stdin.readline
+
+s = input()
+# n = int(input())
+# n, m = map(int, input().split()) #入力を空白区切りで分割し、整数に変換
+# f = float(input())
+# s2, s2, s3 = input().rstrip() #1行文字列を空白区切りで文字列に分割する
+# l = list(map(int, input().split()))
+# a = [int(input()) for _ in range(n)]
+print()
