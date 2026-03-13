b647f3696829085193c324b4b3e1989c5112a23b
wakinoza <wakinoza@gmail.com>
2026-03-13 13:07:00 +0900

    ABC431A.py

---
+++ AtCorder2/ABC431A.py
@@ -0,0 +1,39 @@
+# ABC431A
+import sys
+
+# sys.setrecursionlimit(10**7)
+
+def solve():
+    H, B = map(int, input().split())
+    print(max(0, H - B))
+
+if __name__ == "__main__":
+    solve()
+
+
+# s = input()
+# n = int(input())
+# n, m = map(int, input().split()) #入力を空白区切りで分割し、整数に変換
+# f = float(input())
+# s2, s2, s3 = input().rstrip() #1行文字列を空白区切りで文字列に分割する
+# l = list(map(int, input().split()))
+
+# input = sys.stdin.read().split()
+# it = iter(input)
+
+# # 変数N, M, S, T, Qが受け渡されrる。利用しない変数N, M,Q は _ で受ける。
+# イテレータにすると，インデクスなしでNextで受け取れる
+# _, _, S, T, _ = next(it), next(it), next(it), next(it), next(it)
+
+# 最初のデータを捨てて、残りのデータをリストｔに格納
+# _, *t = map(int, sys.stdin.read().split())
+
+# input_data = sys.stdin.read().split()
+# s = input_data[0]
+# n = int(input_data[0])
+
+# a_iterators = map(input_data[1:])
+# a_iterators_int = map(int, input_data[1:])
+
+# a_list = list(map(input_data[1:]))
+# a_list_int = list(map(int, input_data[1:]))
