12409836ef0003f93403c3cf3d581e9853afb8a0
wakinoza <wakinoza@gmail.com>
2026-03-21 22:41:20 +0900

    Main.java

--- AtCorder2/Main.java
+++ AtCorder2/Main.java
@@ -21,23 +21,19 @@ import java.io.*;
 // ABC
 public class Main {
   public static void main(String[] args) throws IOException {
-    // 1. 入力準備（標準入力をバッファリングして読み込む）
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    //1行に数値が一つの時
+    StringTokenizer firstLine = new StringTokenizer(br.readLine());
+    int b = Integer.parseInt(firstLine.nextToken());
     int n = Integer.parseInt(br.readLine().trim());
 
-    // 数値列（A1 A2 ... AN）の読み込み
-    // StringTokenizerは、読み込んだ1行の文字列を「空白」などで切り分ける役割
     StringTokenizer st = new StringTokenizer(br.readLine());
 
     for (int i = 0; i < n; i++) {
-        // st.nextToken() は、内部のポインタを次の空白まで進めて文字列を返す
-        int a = Integer.parseInt(st.nextToken());
+      int a = Integer.parseInt(st.nextToken());
     }
 
     // 1行に文字列が一つの時
-    String s = br.readLine().trim();
+    char[] s = br.readLine().trim().toCharArray();
 
     // 文字列の列（A1 A2 ... AN）の読み込み
     // StringTokenizerは、読み込んだ1行の文字列を「空白」などで切り分ける役割
