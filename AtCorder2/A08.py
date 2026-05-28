1df89772ed6f7c975d48278116c1b4b77877b929
wakinoza <wakinoza@gmail.com>
2026-05-28 13:29:00 +0900

    鉄則：２次元累積和A08.py　リファクタリング版　

--- AtCorder2/A08.py
+++ AtCorder2/A08.py
@@ -1,33 +1,29 @@
-
-# 鉄則問題集A08（ 2次元リストの累積和）
 import sys
-from itertools import accumulate
-
-# sys.setrecursionlimit(10**7)
 
 def solve():
-    h, w, *other = map(int, sys.stdin.read().split())
-    grid = []
-    for _ in range(h) :
-        line = other[:w]
-        other = other[w:]
-        grid.append(line)
-
-    sumGrid = [[0] * (w + 1) for _ in range(h + 1)]
+    # 入力をすべて一括で取得してイテレータ化
+    it = map(int, sys.stdin.read().split())
+    h, w = next(it), next(it)
+    
+    # 1. 2次元累積和用のテーブル（最初から H+1 x W+1 のサイズで 0 初期化）
+    sum_grid = [[0] * (w + 1) for _ in range(h + 1)]
+    
+    # 2. 元の grid を作らずに、入力ストリームから直接累積和を構築
     for i in range(h):
         for j in range(w):
-            sumGrid[i + 1][j + 1] = sumGrid[i][j + 1] + sumGrid[i + 1][j] - sumGrid[i][j] + grid[i][j]
+            val = next(it)  # その場で次の要素を取得
+            sum_grid[i + 1][j + 1] = sum_grid[i][j + 1] + sum_grid[i + 1][j] - sum_grid[i][j] + val
 
-    answer = []
-    other = iter(other)
-    q = next(other)
-    for _ in range(q) :
-        a, b, c, d = next(other), next(other), next(other), next(other)
-        currentSum = sumGrid[c][d] - sumGrid[a - 1][d] - sumGrid[c][b - 1] + sumGrid[a - 1][b - 1]
-        answer.append(currentSum)
-    print("\n".join(map(str, answer)))
+    # 3. クエリの処理
+    q = next(it)
+    answers = []
+    for _ in range(q):
+        a, b, c, d = next(it), next(it), next(it), next(it)
+        current_sum = sum_grid[c][d] - sum_grid[a - 1][d] - sum_grid[c][b - 1] + sum_grid[a - 1][b - 1]
+        answers.append(str(current_sum))
+        
+    # まとめて出力
+    print("\n".join(answers))
 
 if __name__ == "__main__":
     solve()
-
-
