8ea45ad082ee599f4196f1904faf1d1b46f3280c
wakinoza <wakinoza@gmail.com>
2026-06-06 21:11:11 +0900

    ABC461A.py

---
+++ AtCorder2/ABC461A.py
@@ -0,0 +1,14 @@
+# ABC461A（）
+import sys
+
+# sys.setrecursionlimit(10**7)
+
+def solve():
+    A, D = map(int, input().split())
+    print("Yes" if A <= D else "No")
+
+
+if __name__ == "__main__":
+    solve()
+
+
