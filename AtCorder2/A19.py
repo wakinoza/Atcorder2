8eecf6b5b7fe6ca157e57e84eeae8714deba8868
wakinoza <wakinoza@gmail.com>
2026-06-11 14:10:44 +0900

    鉄則：ナップザック問題の１次元DPA19.py
    ｖ、ｗをｆｏｒ文中でアンパック。ＤＰを異次元配列に変更し、後ろから回す方法にリファクタリング

--- AtCorder2/A19.py
+++ AtCorder2/A19.py
@@ -1,25 +1,20 @@
-# 鉄則問題集A19（DP、ナップザック問題）
+# 鉄則問題集A19（1次元DP、ナップザック問題）
 import sys
 
 def solve():
-    it = iter(map(int, sys.stdin.read().split()))
-    n, w = next(it), next(it)
-    goods = [(next(it), next(it)) for _ in range(n)]
-    dp = [[-1] * (w + 1) for i in range(n + 1)]
-    dp[0][0] = 0
-    answer = 0
-    for i in range(n):
-        for j in range(w + 1):
-            prev_value = dp[i][j]
-            if prev_value >= 0:
-                dp[i + 1][j] = max(prev_value, dp[i + 1][j])
-                next_weight = j + goods[i][0]
-                if next_weight <= w:
-                    next_value = max(dp[i + 1][next_weight], prev_value + goods[i][1])
-                    dp[i + 1][next_weight] = next_value
-                    if answer < next_value:
-                        answer = next_value
-    print(answer)
+    input_data = map(int, sys.stdin.read().split())
+    n, w = next(input_data), next(input_data)
+    dp = [-1] * (w + 1)
+    dp[0] = 0
+    for _ in range(n):
+        weight, value = next(input_data), next(input_data)
+        for j in range(w, weight - 1, -1):
+            if dp[j - weight] != -1:
+                new_value = dp[j - weight] + value
+                if new_value > dp[j]:
+                    dp[j] = new_value
+
+    print(max(dp))
 
 
 if __name__ == "__main__":
