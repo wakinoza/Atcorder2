dc6f334b69e1be29c90b095cadaebbecb39d9586
wakinoza <wakinoza@gmail.com>
2026-07-15 13:06:17 +0900

    鉄則：A38.pyのリファクタリング

--- AtCorder2/A38.py
+++ AtCorder2/A38.py
@@ -4,12 +4,11 @@ import sys
 def solve():
     input_data = map(int, sys.stdin.read().split())
     d, n = next(input_data), next(input_data)
-    days = [0] + [24 for _ in range(d)]
+    days = [24 for _ in range(d)]
     for _ in range(n):
         l, r, h = next(input_data), next(input_data), next(input_data)
-        for x in range(l, r+1):
-            if days[x] > h :
-                days[x] = h
+        for x in range(l-1, r):
+            days[x] = min(days[x], h)
     print(sum(days))
 
 if __name__ == "__main__":
