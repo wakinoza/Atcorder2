95313c2f34f34848e7f7024c45f389dc0f2b9bf2
wakinoza <wakinoza@gmail.com>
2026-09-06 13:34:26 +0900

    ABC474A.py

---
+++ AtCorder2/ABC474A.py
@@ -0,0 +1,15 @@
+# 鉄則問題集ABC474A
+import sys
+
+
+def solve():
+    x = int(input())
+    if x == 1:
+        print(2)
+    elif x == 2:
+        print(3)
+    else:
+        print(1)
+
+if __name__ == "__main__":
+    solve()
